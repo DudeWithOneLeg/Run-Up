8a61b6b2ef32407fb5466cdcb1b562421348767c
dudewithoneleg <galvancromeo@gmail.com>
2023-05-10 18:22:06 -0500

    phase 3 working on /api/restore-user

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const router = express.Router();
+const { setTokenCookie } = require('../../utils/auth.js');
+const { User } = require('../../db/models');
 
 router.post('/test', (req, res) => {
     res.json({
         requestBody: req.body
     })
 })
+
+router.get('/set-token-cookie', async (req, res) => {
+    const user = await User.findOne({
+      where: {
+        username: 'JohnDoe'
+      }
+    });
+    setTokenCookie(res, user);
+    return res.json({ user: user });
+});
 
 module.exports = router
