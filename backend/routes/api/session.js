8ceb94576c07ed222cf09292dc70a192ced66e0c
dudewithoneleg <galvancromeo@gmail.com>
2023-05-11 16:01:32 -0500

    Add a backend endpoint to get the current user session

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -10,6 +10,23 @@ const { User } = require('../../db/models')
 
 // router.use(restoreUser)
 
+router.get(
+    '/',
+    (req, res) => {
+      const { user } = req;
+      if (user) {
+        const safeUser = {
+          id: user.id,
+          email: user.email,
+          username: user.username,
+        };
+        return res.json({
+          user: safeUser
+        });
+      } else return res.json({ user: null });
+    }
+);
+
 router.post(
     '/',
     async (req, res, next) => {
--- backend/utils/auth.js
+++ backend/utils/auth.js
@@ -8,58 +8,58 @@ const { secret, expiresIn } = jwtConfig;
 const setTokenCookie = (res, user) => {
     // Create the token.
     const safeUser = {
-      id: user.id,
-      email: user.email,
-      username: user.username,
+        id: user.id,
+        email: user.email,
+        username: user.username,
     };
     const token = jwt.sign(
-      { data: safeUser },
-      secret,
-      { expiresIn: parseInt(expiresIn) } // 604,800 seconds = 1 week
+        { data: safeUser },
+        secret,
+        { expiresIn: parseInt(expiresIn) } // 604,800 seconds = 1 week
     );
 
     const isProduction = process.env.NODE_ENV === "production";
 
     // Set the token cookie
     res.cookie('token', token, {
-      maxAge: expiresIn * 1000, // maxAge in milliseconds
-      httpOnly: true,
-      secure: isProduction,
-      sameSite: isProduction && "Lax"
+        maxAge: expiresIn * 1000, // maxAge in milliseconds
+        httpOnly: true,
+        secure: isProduction,
+        sameSite: isProduction && "Lax"
     });
 
     return token;
-  };
+};
 
-  const restoreUser = (req, res, next) => {
+const restoreUser = (req, res, next) => {
     // token parsed from cookies
     const { token } = req.cookies;
     req.user = null;
 
     return jwt.verify(token, secret, null, async (err, jwtPayload) => {
-      if (err) {
+        if (err) {
+            return next();
+        }
+
+        try {
+            const { id } = jwtPayload.data;
+            req.user = await User.findByPk(id, {
+                attributes: {
+                    include: ['email', 'createdAt', 'updatedAt']
+                }
+            });
+        } catch (e) {
+            res.clearCookie('token');
+            return next();
+        }
+
+        if (!req.user) res.clearCookie('token');
+
         return next();
-      }
-
-      try {
-        const { id } = jwtPayload.data;
-        req.user = await User.findByPk(id, {
-          attributes: {
-            include: ['email', 'createdAt', 'updatedAt']
-          }
-        });
-      } catch (e) {
-        res.clearCookie('token');
-        return next();
-      }
-
-      if (!req.user) res.clearCookie('token');
-
-      return next();
     });
-  };
+};
 
-  // If there is no current user, return an error
+// If there is no current user, return an error
 const requireAuth = function (req, _res, next) {
     if (req.user) return next();
 
@@ -68,6 +68,6 @@ const requireAuth = function (req, _res, next) {
     err.errors = { message: 'Authentication required' };
     err.status = 401;
     return next(err);
-  }
+}
 
-  module.exports = { setTokenCookie, restoreUser, requireAuth };
+module.exports = { setTokenCookie, restoreUser, requireAuth };
