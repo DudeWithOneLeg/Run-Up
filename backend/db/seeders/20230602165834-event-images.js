5089e86c0ebd58c60045d09ee44eff7bbd7726a6
dudewithoneleg <galvancromeo@gmail.com>
2023-07-24 17:22:07 -0500

    diverse event images

--- backend/db/seeders/20230602165834-event-images.js
+++ backend/db/seeders/20230602165834-event-images.js
@@ -22,52 +22,52 @@ module.exports = {
    const validEventImages = [
     {
       eventId: 1,
-      url: 'https://www.shutterstock.com/shutterstock/photos/730017625/display_1500/stock-photo-evening-stadium-arena-soccer-field-defocus-background-730017625.jpg',
+      url: 'https://static4.depositphotos.com/1010683/285/i/450/depositphotos_2858508-stock-photo-sport-stadium.jpg',
       preview: true
     },
     {
       eventId: 2,
-      url: 'https://www.shutterstock.com/shutterstock/photos/299796638/display_1500/stock-photo-athletics-stadium-with-track-and-grass-field-at-corner-view-sport-theme-render-illustration-299796638.jpg',
+      url: 'https://st.depositphotos.com/2350297/3285/i/450/depositphotos_32853181-stock-photo-track-and-field-park.jpg',
       preview: true
     },
     {
       eventId: 3,
-      url: 'https://www.shutterstock.com/shutterstock/photos/306123788/display_1500/stock-photo-athlete-track-or-running-track-with-nice-scenic-306123788.jpg',
+      url: 'https://st2.depositphotos.com/5083063/7580/i/450/depositphotos_75809967-stock-photo-red-race-for-running.jpg',
       preview: true
     },
     {
       eventId: 4,
-      url: 'https://www.shutterstock.com/shutterstock/photos/306123788/display_1500/stock-photo-athlete-track-or-running-track-with-nice-scenic-306123788.jpg',
+      url: 'https://st2.depositphotos.com/2389105/5353/i/450/depositphotos_53536963-stock-photo-athletics-track.jpg',
       preview: true
     },
     {
       eventId: 5,
-      url: 'https://www.shutterstock.com/shutterstock/photos/306123788/display_1500/stock-photo-athlete-track-or-running-track-with-nice-scenic-306123788.jpg',
+      url: 'https://static4.depositphotos.com/1004999/269/i/450/depositphotos_2693490-stock-photo-beautiful-park.jpg',
       preview: true
     },
     {
       eventId: 6,
-      url: 'https://www.shutterstock.com/shutterstock/photos/306123788/display_1500/stock-photo-athlete-track-or-running-track-with-nice-scenic-306123788.jpg',
+      url: 'https://st4.depositphotos.com/10459226/23194/i/450/depositphotos_231948278-stock-photo-summer-park-path-green-trees.jpg',
       preview: true
     },
     {
       eventId: 7,
-      url: 'https://www.shutterstock.com/shutterstock/photos/306123788/display_1500/stock-photo-athlete-track-or-running-track-with-nice-scenic-306123788.jpg',
+      url: 'https://st3.depositphotos.com/16149430/19181/i/450/depositphotos_191815784-stock-photo-park.jpg',
       preview: true
     },
     {
       eventId: 8,
-      url: 'https://www.shutterstock.com/shutterstock/photos/306123788/display_1500/stock-photo-athlete-track-or-running-track-with-nice-scenic-306123788.jpg',
+      url: 'https://st2.depositphotos.com/1011549/43183/i/450/depositphotos_431834128-stock-photo-beautiful-meadow-footpath-park.jpg',
       preview: true
     },
     {
       eventId: 9,
-      url: 'https://www.shutterstock.com/shutterstock/photos/306123788/display_1500/stock-photo-athlete-track-or-running-track-with-nice-scenic-306123788.jpg',
+      url: 'https://st.depositphotos.com/1695227/4813/i/450/depositphotos_48132517-stock-photo-deep-forest-stream-with-crystal.jpg',
       preview: true
     },
     {
       eventId: 10,
-      url: 'https://www.shutterstock.com/shutterstock/photos/306123788/display_1500/stock-photo-athlete-track-or-running-track-with-nice-scenic-306123788.jpg',
+      url: 'https://st4.depositphotos.com/10459226/23194/i/450/depositphotos_231948658-stock-photo-summer-park-path-green-trees.jpg',
       preview: true
     },
     {
