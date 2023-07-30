b973ffe17ee1f10f42d8feaf449a6db832425248
dudewithoneleg <galvancromeo@gmail.com>
2023-07-29 22:26:12 -0500

    create venue

--- frontend/src/components/404/index.js
+++ frontend/src/components/404/index.js
@@ -3,9 +3,12 @@ import './index.css'
 export default function PageNotFound() {
     return (
         <div id='page-not-found'>
+            <div>
+                <h1>4</h1>
+            <img id ='steve' src='/images/steve.jpg' alt='steve'></img>
             <h1>4</h1>
-            <img src='/images/steve.jpg' alt='steve'></img>
-            <h1>4</h1>
+            </div>
+
         </div>
     )
 }
